f254871252d3b87e899d383332cf6002380e7047
Stepan Tsiglevkin <stepan.tsiglevkin@mail.ru>
2022-12-25 22:47:59 +0500

    views are done.

--- phones/views.py
+++ phones/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
+    """Пустой запрос делает редирект на каталог"""
     return redirect('catalog')
 
 
 def show_catalog(request):
+    """Обработчик сортирует данные по стоимости и имени. если нет сортировки, выводит базовый порядок"""
     template = 'catalog.html'
-    context = {}
+    sorting = request.GET.get('sort')
+    if sorting == 'name':
+        phones = Phone.objects.order_by(sorting)  # order_by сортирует по заданному полю.
+    elif sorting == 'min_price':
+        phones = Phone.objects.order_by('-price')  # '-' сортирует от минимума
+    elif sorting == 'max_price':
+        phones = Phone.objects.order_by('price')
+    else:
+        phones = Phone.objects.all()
+
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    """Функция показывает информацию телефона по конкретному slug'у."""
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
